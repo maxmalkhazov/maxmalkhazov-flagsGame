c392cc3f5ab6767ce2901b68e5d73e10b30a714f
maxmalkhazov <maxmal86@gmail.com>
2017-11-12 13:09:52 -0600

    Initial commit

---
+++ resources/js/flagsGame.js
@@ -0,0 +1,364 @@
+var flags = [
+    "resources/flags/afghanistan.png",
+    "resources/flags/Albania.png",
+    "resources/flags/Algeria.png",
+    "resources/flags/andorra.png",
+    "resources/flags/angola.png",
+    "resources/flags/Antigua and Barbuda.png",
+    "resources/flags/argentina.png",
+    "resources/flags/armenia.png",
+    "resources/flags/australia.png",
+    "resources/flags/austria.png",
+    "resources/flags/Azerbaijan.png",
+    "resources/flags/Bahamas.png",
+    "resources/flags/Bahrain.png",
+    "resources/flags/Bangladesh.png",
+    "resources/flags/Barbados.png",
+    "resources/flags/Belarus.png",
+    "resources/flags/belgium.png",
+    "resources/flags/Belize.png",
+    "resources/flags/Benin.png",
+    "resources/flags/Bhutan.png",
+    "resources/flags/Bolivia.png",
+    "resources/flags/Bosnia and Herzegovina.png",
+    "resources/flags/Botswana.png",
+    "resources/flags/brazil.png",
+    "resources/flags/Brunei.png",
+    "resources/flags/bulgaria.png",
+    "resources/flags/Burkina Faso.png",
+    "resources/flags/Burundi.png",
+    "resources/flags/Cambodia.png",
+    "resources/flags/Cameroon.png",
+    "resources/flags/canada.png",
+    "resources/flags/Cape Verde.png",
+    "resources/flags/Central African Republic.png",
+    "resources/flags/Chad.png",
+    "resources/flags/Chile.png",
+    "resources/flags/China.png",
+    "resources/flags/Colombia.png",
+    "resources/flags/Comoros.png",
+    "resources/flags/Congo.png",
+    "resources/flags/Costa Rica.png",
+    "resources/flags/Croatia.png",
+    "resources/flags/cuba.png",
+    "resources/flags/cyprus.png",
+    "resources/flags/Czech Republic.png",
+    "resources/flags/Democratic Republic of the Congo.png",
+    "resources/flags/Denmark.png",
+    "resources/flags/Djibouti.png",
+    "resources/flags/Dominica.png",
+    "resources/flags/Dominican Republic.png",
+    "resources/flags/Ecuador.png",
+    "resources/flags/Egypt.png",
+    "resources/flags/El Salvador.png",
+    "resources/flags/Equatorial Guinea.png",
+    "resources/flags/Eritrea.png",
+    "resources/flags/Estonia.png",
+    "resources/flags/Ethiopia.png",
+    "resources/flags/FS Micronesia.png",
+    "resources/flags/Fiji.png",
+    "resources/flags/Finland.png",
+    "resources/flags/France.png",
+    "resources/flags/Gabon.png",
+    "resources/flags/Gambia.png",
+    "resources/flags/georgia.png",
+    "resources/flags/germany.png",
+    "resources/flags/Ghana.png",
+    "resources/flags/greece.png",
+    "resources/flags/Grenada.png",
+    "resources/flags/Guatemala.png",
+    "resources/flags/Guinea.png",
+    "resources/flags/Guinea-Bissau.png",
+    "resources/flags/Guyana.png",
+    "resources/flags/Haiti.png",
+    "resources/flags/Honduras.png",
+    "resources/flags/Hungary.png",
+    "resources/flags/Iceland.png",
+    "resources/flags/India.png",
+    "resources/flags/Indonesia.png",
+    "resources/flags/Iran.png",
+    "resources/flags/Iraq.png",
+    "resources/flags/ireland.png",
+    "resources/flags/israel.png",
+    "resources/flags/italy.png",
+    "resources/flags/Ivory Coast.png",
+    "resources/flags/jamaica.png",
+    "resources/flags/japan.png",
+    "resources/flags/Jordan.png",
+    "resources/flags/Kazakhstan.png",
+    "resources/flags/Kenya.png",
+    "resources/flags/Kingdom of the Netherlands.png",
+    "resources/flags/Kiribati.png",
+    "resources/flags/Kosovo.png",
+    "resources/flags/Kuwait.png",
+    "resources/flags/Kyrgyzstan.png",
+    "resources/flags/Laos.png",
+    "resources/flags/Latvia.png",
+    "resources/flags/lebanon.png",
+    "resources/flags/Lesotho.png",
+    "resources/flags/Liberia.png",
+    "resources/flags/Libya.png",
+    "resources/flags/Liechtenstein.png",
+    "resources/flags/Lithuania.png",
+    "resources/flags/Luxembourg.png",
+    "resources/flags/Macedonia.png",
+    "resources/flags/Madagascar.png",
+    "resources/flags/Malawi.png",
+    "resources/flags/Malaysia.png",
+    "resources/flags/Maldives.png",
+    "resources/flags/Mali.png",
+    "resources/flags/Malta.png",
+    "resources/flags/Marshall Islands.png",
+    "resources/flags/Mauritania.png",
+    "resources/flags/Mauritius.png",
+    "resources/flags/mexico.png",
+    "resources/flags/Moldova.png",
+    "resources/flags/Monaco.png",
+    "resources/flags/Mongolia.png",
+    "resources/flags/Montenegro.png",
+    "resources/flags/Morocco.png",
+    "resources/flags/Mozambique.png",
+    "resources/flags/Myanmar.png",
+    "resources/flags/Namibia.png",
+    "resources/flags/Nauru.png",
+    "resources/flags/Nepal.png",
+    "resources/flags/new zealand.png",
+    "resources/flags/Nicaragua.png",
+    "resources/flags/Niger.png",
+    "resources/flags/Nigeria.png",
+    "resources/flags/North Korea.png",
+    "resources/flags/Norway.png",
+    "resources/flags/Oman.png",
+    "resources/flags/pakistan.png",
+    "resources/flags/Palau.png",
+    "resources/flags/Panama.png",
+    "resources/flags/Papua New Guinea.png",
+    "resources/flags/Paraguay.png",
+    "resources/flags/Peru.png",
+    "resources/flags/Philippines.png",
+    "resources/flags/Poland.png",
+    "resources/flags/Portugal.png",
+    "resources/flags/Qatar.png",
+    "resources/flags/Romania.png",
+    "resources/flags/russia.png",
+    "resources/flags/Rwanda.png",
+    "resources/flags/Sahrawi Arab Democratic Republic.png",
+    "resources/flags/Saint Kitts and Nevis.png",
+    "resources/flags/Saint Lucia.png",
+    "resources/flags/Saint Vincent and the Grenadines.png",
+    "resources/flags/Samoa.png",
+    "resources/flags/San Marino.png",
+    "resources/flags/Sao Tome and Principe.png",
+    "resources/flags/saudi arabia.png",
+    "resources/flags/Senegal.png",
+    "resources/flags/Serbia.png",
+    "resources/flags/Seychelles.png",
+    "resources/flags/Sierra Leone.png",
+    "resources/flags/Singapore.png",
+    "resources/flags/slovakia.png",
+    "resources/flags/slovenia.png",
+    "resources/flags/Solomon Islands.png",
+    "resources/flags/Somalia.png",
+    "resources/flags/South Africa.png",
+    "resources/flags/South Korea.png",
+    "resources/flags/spain.png",
+    "resources/flags/Sri Lanka.png",
+    "resources/flags/Sudan.png",
+    "resources/flags/Suriname.png",
+    "resources/flags/Swaziland.png",
+    "resources/flags/Sweden.png",
+    "resources/flags/Switzerland.png",
+    "resources/flags/Syria.png",
+    "resources/flags/Taiwan.png",
+    "resources/flags/Tajikistan.png",
+    "resources/flags/Tanzania.png",
+    "resources/flags/Thailand.png",
+    "resources/flags/Timor-Leste.png",
+    "resources/flags/Togo.png",
+    "resources/flags/Tonga.png",
+    "resources/flags/Trinidad and Tobago.png",
+    "resources/flags/Tunisia.png",
+    "resources/flags/Turkey.png",
+    "resources/flags/Turkmenistan.png",
+    "resources/flags/Tuvalu.png",
+    "resources/flags/Uganda.png",
+    "resources/flags/Ukraine.png",
+    "resources/flags/United Arab Emirates.png",
+    "resources/flags/united kingdom.png",
+    "resources/flags/united states.png",
+    "resources/flags/uruguay.png",
+    "resources/flags/Uzbekistan.png",
+    "resources/flags/Vanuatu.png",
+    "resources/flags/Vatican City.png",
+    "resources/flags/Venezuela.png",
+    "resources/flags/Vietnam.png",  
+    "resources/flags/Yemen.png",  
+    "resources/flags/Zambia.png",  
+    "resources/flags/Zimbabwe.png",  
+];
+
+var cntDisplay = document.querySelector(".cntDisplay");
+var squares = document.querySelectorAll(".square");
+var img = document.querySelectorAll(".flagImg");
+var h1 = document.querySelector("h1");
+var backColor = document.querySelector(".backColor");
+var messageDisplay = document.querySelector(".message");
+var resetButton = document.querySelector(".reset");
+var aboutButton = document.querySelector(".about");
+var about = document.querySelector("#about");
+var container = document.querySelector("#container");
+var track = document.querySelector(".track");
+var scoreDisplay = document.querySelector(".score");
+var score = 0;
+var rFlags = generateRandomFlag(4);
+
+
+// loop through the image squares
+for (var i = 0; i < img.length; i++) {
+	// assign the flags to the images
+	img[i].setAttribute("src", rFlags[i]);
+	img[i].addEventListener("click", function() {
+		// assigned the clicked flag to a variable
+	var clickedFlag = this.getAttribute("src");
+	// compare if the clicked flag equals to the displayed country
+	if (clickedFlag.includes(cntDisplay.textContent)) {
+		messageDisplay.textContent = "Correct!";
+		resetButton.textContent = "Play Again?"
+		// if correct change h1 background to the correct country flag
+		backImage(clickedFlag);
+		// if correct change all squares to the correct country flag
+		changeSquares(clickedFlag);
+		// add one to score and update display
+		resetButton.classList.add("selected");
+		score++;
+		scoreDisplay.textContent = score;
+		scoreDisplay.classList.add("selected");
+		scoreDisplay.style.color = "white";
+		// prevent clicking on correct image more than once
+		for (var i = 0; i < img.length; i++) {
+			img[i].classList.add("noClick");
+		}
+	}
+	else {
+		// if not correct hide the clicked flag
+		this.style.visibility = "hidden";
+		this.style.opacity = "0";
+		messageDisplay.textContent = "Try Again!";
+		score = 0;
+		scoreDisplay.textContent = score;
+		scoreDisplay.classList.remove("selected");
+		scoreDisplay.style.color = "red";
+	}
+		
+	});
+}
+
+// CHANGE H1 BACKGROUND TO THE CORRECT IMAGE WHEN THE ANSWER IS CORRECT
+function backImage(image) {
+	h1.style.backgroundImage = 'url("'+ image +'")';
+	h1.style.backgroundPosition = "center";
+	h1.style.backgroundSize = "cover";
+	h1.style.backgroundRepeat = "no-repeat";
+	h1.style.color = "black";
+	h1.classList.add("special");
+	backColor.classList.add("setBack");
+}
+
+// CHANGE ALL IMAGES TO THE CORRECT IMAGE WHEN THE ANSWER IS CORRECT
+function changeSquares(flag) {
+	for (var i = 0; i < img.length; i++) {
+		img[i].style.visibility = "visible";
+		img[i].style.opacity = "1";
+		img[i].setAttribute("src", flag);
+	}
+}
+
+// GENERATE RANDOM FLAG
+function randomFlag() {
+	var randomFlag = Math.floor(Math.random() * flags.length);
+	return flags[randomFlag];
+}
+
+// PUSH 4 RANDOM FLAGS INTO AN ARRAY AND MAKE SURE ONE FLAG DOESN'T GET PUSHED TWICE
+function generateRandomFlag(num) {
+	var arr = [];
+	var reachNum = false;
+	var ctr = 0;
+	while (!reachNum) {
+		var flag = randomFlag();
+		if (arr.indexOf(flag) === -1) {
+			arr.push(flag);
+			ctr++;
+		}
+		if (ctr === num) {
+			reachNum = true;
+		}
+	}
+	return arr;
+}
+
+/*
+// PUSH 4 RANDOM FLAGS INTO AN ARRAY --- ALTERNATIVE WAY
+function generateRandomFlag(num) {
+    return flags
+    .slice() // copy the flags array because .sort mutates the source array
+    .sort(function() { 
+    return Math.random() - 0.5;
+    }) // shuffle the copied array
+    .slice(-num); // get the LAST "num" values of the shuffled array
+}
+
+*/
+
+// NEED TO PULL RANDOM COUNTRY NAME OUT OF THE ARRAY THAT CONTAINS 4 FLAGS
+function pickCountry() {
+	var randomCountry = Math.floor(Math.random() * rFlags.length);
+	return rFlags[randomCountry].slice(16, -4);
+}
+
+cntDisplay.textContent = pickCountry();
+
+// CREATE PLAY AGAIN BUTTON
+resetButton.addEventListener("click", function() {
+// generate all new flags
+	rFlags = generateRandomFlag(4);
+	// change display country to picked country
+	cntDisplay.textContent = pickCountry();
+	// make squares visible
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.display = "block";
+		about.style.display = "none";
+	}
+	// assign the new flags to the image squares
+	for (var i = 0; i < img.length; i++) {
+	    img[i].setAttribute("src", rFlags[i]);
+	    img[i].style.visibility = "visible";
+	    img[i].style.opacity = "1";
+	    img[i].classList.remove("noClick");
+	}
+	resetButton.innerHTML = '<i class="fa fa-refresh" aria-hidden="true"></i>' + " " + "Shuffle";
+	messageDisplay.textContent = "";
+	h1.style.backgroundImage = "none";
+	h1.style.color = "white";
+	h1.classList.remove("special");
+	backColor.classList.remove("setBack");
+	container.classList.remove("setBack");
+	about.classList.remove("setForward");
+	resetButton.classList.remove("selected");
+	aboutButton.classList.remove("selected");
+});
+
+// ABOUT BUTTON
+aboutButton.addEventListener("click", function() {
+	container.classList.toggle("setBack");
+	about.classList.toggle("setForward");
+	aboutButton.classList.toggle("selected");
+});
+
+
+
+
+
+
+
+
